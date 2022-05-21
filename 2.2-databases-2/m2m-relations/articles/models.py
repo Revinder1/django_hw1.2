d0ca0213f9382d0551776ef3cf48c990ca08788e
Yaroslav Bogdanov <litle32@ya.ru>
2022-05-22 00:02:45 +0500

    added m2m-relations hw

--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+    articles = models.ManyToManyField('Article', through='Scope')
+
+    def __str__(self):
+        return self.name
+
+
 class Article(models.Model):
 
     title = models.CharField(max_length=256, verbose_name='Название')
@@ -14,3 +22,9 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Scope(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE,  related_name='scopes')
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, verbose_name='Раздел', related_name='scopes')
+    is_main = models.BooleanField(verbose_name='Основной')
